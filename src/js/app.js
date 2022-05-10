13922acf11934b5c799d465ecd76e2e61424755d
maluy <artem.evsiukov1@gmail.com>
2022-05-10 17:29:09 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,7 @@
 import burgerMenu from './components/burger-menu';
 import swiperSlider from './components/slider';
 import tabs from './components/tabs';
+import initSelects from './components/select';
 
 (($) => {
   // When DOM is ready
@@ -15,41 +16,63 @@ import tabs from './components/tabs';
     tabs.init();
     burgerMenu.init();
     swiperSlider.init();
+    initSelects();
   });
 })(jQuery);
 
-$( document ).ready(function() {
+$(document).ready(function () {
   // for active element`s menu addClass('active')
   var pathname = window.location.pathname;
-  $(".nav__list .nav__item a").each(
-    function(){
-      if ($(this).attr('href') == pathname){
-        $(this).parent('.nav__item').addClass("active");
-      }
+  $('.nav__list .nav__item a').each(function () {
+    if ($(this).attr('href') == pathname) {
+      $(this).parent('.nav__item').addClass('active');
     }
-  );
+  });
   //for form focus input
-  $('input').on('focus', function(){
+  $('input').on('focus', function () {
     $(this).parent('.form-group').children('label').addClass('focus');
-  })
+  });
   //for form focusout input
-  $('input').focusout(function() {
-    if ($(this).val() == ""){
-      $(this).parent('.form-group').children('.validation-error').addClass("active");
-      $(this).addClass("danger");
+  $('input').focusout(function () {
+    if ($(this).val() == '') {
+      $(this).parent('.form-group').children('.validation-error').addClass('active');
+      $(this).addClass('danger');
       $(this).parent('.form-group').children('label').removeClass('focus');
+    } else if ($(this).parent('.form-group').children('.validation-error').hasClass('active')) {
+      $(this).parent('.form-group').children('.validation-error').removeClass('active');
+      $(this).removeClass('danger');
     }
-    else if ($(this).parent('.form-group').children('.validation-error').hasClass("active")){
-      $(this).parent('.form-group').children('.validation-error').removeClass("active");
-      $(this).removeClass("danger");
-    }
-  })
+  });
   //for form focus textarea
-  $('textarea').on('focus', function(){
+  $('textarea').on('focus', function () {
     $(this).parent('.form-group').children('label').addClass('focus');
-  })
+  });
   //for form focusout textarea
-  $('textarea').focusout(function() {
+  $('textarea').focusout(function () {
     $(this).parent('.form-group').children('label').removeClass('focus');
-  })
+  });
+});
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+  dragToClose: false,
+
+  Toolbar: false,
+  closeButton: 'top',
+
+  Image: {
+    zoom: false,
+  },
+
+  on: {
+    initCarousel: (fancybox) => {
+      const slide = fancybox.Carousel.slides[fancybox.Carousel.page];
+
+      fancybox.$container.style.setProperty('--bg-image', `url("${slide.$thumb.src}")`);
+    },
+    'Carousel.change': (fancybox, carousel, to, from) => {
+      const slide = carousel.slides[to];
+
+      fancybox.$container.style.setProperty('--bg-image', `url("${slide.$thumb.src}")`);
+    },
+  },
 });
--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -11,6 +11,19 @@ const swiperSlider = (() => {
     },
   });
 
+  const portfolioSwiper = new Swiper('.js-portfolio-slider', {
+    slidesPerView: 1,
+  
+    speed: 1000,
+    centerInsufficientSlides: true,
+    autoplay: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  });
+
   const init = () => {};
 
   return {
